876add284f68cb9117f47eb9e6cb6414f5d8a331
AndersonPaulaSilva <48701676+AndersonPaulaSilva@users.noreply.github.com>
2019-09-07 17:24:26 -0300

    Initial commit

---
+++ src/main/java/net/anderson/BootIonicApplication.java
@@ -0,0 +1,13 @@
+package net.anderson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootIonicApplication.class, args);
+	}
+
+}
